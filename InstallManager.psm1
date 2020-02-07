2a3687ab6bbe25e638dac6d2848b175adad8819b
Mike Campbell <mike@campbellnetwork.net>
2020-02-07 10:45:34 -0500

    explicitly specify functions to run at module import

--- InstallManager.psm1
+++ InstallManager.psm1
@@ -8,7 +8,20 @@ enum InstallManager { Chocolatey; Git; PowerShellGet; Manual }
 ###############################################################################################
 # Module Functions
 ###############################################################################################
-$AllFunctionFiles = Get-ChildItem -Recurse -File -Filter *.ps1 -Path $(Join-Path -Path $PSScriptRoot -ChildPath 'Functions')
+$AllFunctionFiles = (Get-ChildItem -Recurse -File -Filter *.ps1 -Path $(Join-Path -Path $PSScriptRoot -ChildPath 'Functions')).where({
+    $_.BaseName -in @(
+      'Get-IMChocoInstall'
+      'Get-IMPowerShellGetInstall'
+      'Get-IMSystemUninstallEntry'
+      'Get-IMDefinition'
+      'Import-IMModuleConfig'
+      'Update-IMInstall'
+      'New-IMDefinition'
+      'Remove-IMDefinition'
+      'Set-IMDefinition'
+    )
+})
+
 $AllFunctionFiles.foreach( { . $_.fullname })
 ###############################################################################################
 # Module Removal
