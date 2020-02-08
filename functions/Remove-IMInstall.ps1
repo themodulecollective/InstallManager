9435654e197b55cf486cebd9d6e205a7a3d8e035
Mike Campbell <mike@exactsolutions.biz>
2020-02-07 19:19:35 -0500

    update stub file for remove-iminstall

--- functions/Remove-IMInstall.ps1
+++ functions/Remove-IMInstall.ps1
@@ -1,5 +1,20 @@
-function Verb-Noun
+function Remove-IMInstall
 {
+  <#
+  .SYNOPSIS
+    Short description
+  .DESCRIPTION
+    Long description
+  .EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+  .INPUTS
+    Inputs (if any)
+  .OUTPUTS
+    Output (if any)
+  .NOTES
+    General notes
+  #>
   [CmdletBinding()]
   param (
 
