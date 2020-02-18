e4edae1842feed5fb77a65946cdfd421fc166429
Mike Campbell <mike@exactsolutions.biz>
2020-02-17 23:55:05 -0500

    working on module file arrangement for pre-build and build

--- InstallManager/InstallManager.psm1
+++ InstallManager/InstallManager.psm1
@@ -1,4 +1,23 @@
 #Requires -Version 5.1
+
+$ModuleName = $MyInvocation.MyCommand.Name.Replace(".psm1", "")
+#Write-Information -MessageData "Module Name is $ModuleName" -InformationAction Continue
+$ModuleManifest = Join-Path -Path $PSScriptRoot -ChildPath $($Script:ModuleName + '.psd1')
+#Write-Information -MessageData "Module Manifest is $ModuleManifest" -InformationAction Continue
+$ModuleFunctionFiles = @(
+  'Export-IMDefinition'
+  'Get-IMChocoInstall'
+  'Get-IMPowerShellGetInstall'
+  'Get-IMSystemUninstallEntry'
+  'Get-IMDefinition'
+  'Import-IMDefinition'
+  'Import-IMConfiguration'
+  'Update-IMInstall'
+  'New-IMDefinition'
+  'Remove-IMDefinition'
+  'Set-IMDefinition'
+)
+
 . $PSScriptRoot\scripts\BeginImport.ps1
 . $PSScriptRoot\scripts\LoadFunctions.ps1
 . $PSScriptRoot\scripts\AtRemoval.ps1
