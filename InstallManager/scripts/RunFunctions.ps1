04dd6993a3ec002f84d0c8b59112df6e35d264e6
Mike Campbell <mike@exactsolutions.biz>
2020-03-04 22:34:44 -0500

    add tab completion for definition names; increment version

--- InstallManager/scripts/RunFunctions.ps1
+++ InstallManager/scripts/RunFunctions.ps1
@@ -1,4 +1,34 @@
 ###############################################################################################
-# Module Ready for User
+# Import User's Configuration
 ###############################################################################################
 Import-IMConfiguration
+###############################################################################################
+# Setup Tab Completion
+###############################################################################################
+# Tab Completions for IM Definition Names
+$ImDefinitionsScriptBlock = {
+  param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+  $MyParams = @{ }
+  if ($null -ne $fakeBoundParameter.InstallManager)
+  {
+    $MyParams.InstallManager = $fakeBoundParameter.InstallManager
+  }
+  if ($null -ne $wordToComplete)
+  {
+    $MyParams.Name = $wordToComplete + '*'
+  }
+  $MyNames = Get-IMDefinition @MyParams |
+    Select-Object -expandProperty Name
+
+  foreach ($n in $MyNames)
+  {
+    [System.Management.Automation.CompletionResult]::new($n, $n, 'ParameterValue', $n)
+  }
+}
+
+Register-ArgumentCompleter -CommandName @(
+  'Get-IMDefinition'
+  'Set-IMDefinition'
+  'Remove-IMDefinition'
+  'Update-IMInstall'
+) -ParameterName 'Name' -ScriptBlock $ImDefinitionsScriptBlock
